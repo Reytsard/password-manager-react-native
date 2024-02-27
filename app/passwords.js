6c96e55f23b603946c80dd8e546b093e7bb83949
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2024-02-27 16:46:58 +0800

    Add Search Bar, Cards for each Credentials

--- app/passwords.js
+++ app/passwords.js
@@ -1,9 +1,153 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useMemo, useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from "react-native";
 
 export default function Page() {
+  const [passwords, setPasswords] = useState([
+    {
+      id: 4,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 5,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 1,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 3,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 2,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 6,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 7,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 8,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 9,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 10,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 11,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 12,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 13,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 14,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 15,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 16,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 17,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 18,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 19,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+    {
+      id: 20,
+      name: "Valorant 1",
+      email: "myppsmollikeurs",
+      password: "AsdfhjlkZxcvbnm0001",
+    },
+  ]);
+  const passwordCards = useMemo(() => {
+    return passwords.map((card) => (
+      <View key={card.id}>
+        <Text>{card.name}</Text>
+        <Text>Email: {card.email}</Text>
+        <Text>Password: {card.password}</Text>
+      </View>
+    ));
+  }, [passwords]);
   return (
     <View style={styles.container}>
+      <TextInput placeholder="search" />
+      <TouchableHighlight style={styles.addPasswordButton}>
+        <Text>Add Credentials</Text>
+      </TouchableHighlight>
       <Text>Passwords</Text>
+      <ScrollView>{passwordCards}</ScrollView>
     </View>
   );
 }
@@ -19,5 +163,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     maxWidth: 960,
     marginHorizontal: "auto",
+  },
+  addPasswordButton: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    maxWidth: 960,
+    height: 40,
+    width: "100%",
+    backgroundColor: "gray",
+    borderRadius: 10,
   },
 });
