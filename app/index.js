1ddfcaf9da84271d83a51379adbd1ccaed345627
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2024-02-27 10:57:08 +0800

    Add password Page

--- app/index.js
+++ app/index.js
@@ -1,23 +1,44 @@
+import { useRouter } from "expo-router";
 import { useState } from "react";
 import {
   Keyboard,
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
 
 export default function Page() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [passwordInput, setPasswordInput] = useState("");
   const PASSWORD = "123";
+  const router = useRouter();
+
+  const verifyPassword = () => {
+    if (passwordInput === PASSWORD) {
+      setIsLoggedIn(true);
+      router.replace("/passwords");
+    }
+  };
   return (
     <View style={styles.container}>
       <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
         <View style={styles.main}>
           <Text style={styles.title}>Password Manager</Text>
           <Text style={styles.subtitle}>Password:</Text>
-          <TextInput secureTextEntry={true} style={styles.passwordInput} />
+          <TextInput
+            secureTextEntry={true}
+            style={styles.passwordInput}
+            onChangeText={(e) => setPasswordInput(e)}
+          />
+          <TouchableOpacity
+            onPress={verifyPassword}
+            style={styles.buttonContainer}
+          >
+            <Text style={styles.loginButton}>Log In</Text>
+          </TouchableOpacity>
         </View>
       </TouchableWithoutFeedback>
     </View>
@@ -38,7 +59,6 @@ const styles = StyleSheet.create({
   },
   title: {
     textAlign: "center",
-
     fontSize: 64,
     fontWeight: "bold",
   },
@@ -53,5 +73,18 @@ const styles = StyleSheet.create({
     height: 36,
     color: "#FFFFFF",
     backgroundColor: "#7F7F7F",
+  },
+  loginButton: {
+    height: "36px",
+  },
+  buttonContainer: {
+    marginTop: 20,
+    maxWidth: 960,
+    maxHeight: 80,
+    height: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "cyan",
   },
 });
