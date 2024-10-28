30149fec622af1f2d990e4028af5d0eb0e9bc6e5
RONALD DE MESA <ronaldreyjr061201@gmail.com>
2024-10-28 21:26:34 +0800

    finish id edit

--- components/BackButton.js
+++ components/BackButton.js
@@ -18,14 +18,15 @@ const styles = StyleSheet.create({
   backButton: {
     position: "absolute",
     top: 0,
-    left: 0,
+    left: 10,
     paddingLeft: 15,
     fontSize: 42,
+    // height: 200,
   },
   DarkBackButton: {
     position: "absolute",
     top: 0,
-    left: 0,
+    left: 10,
     paddingLeft: 15,
     fontSize: 42,
     color: "white",
